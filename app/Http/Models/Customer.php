088d968bf615fd920a345bb9b17f6af45f168d95
Vinicius Bassalobre <=>
2021-02-24 20:40:43 -0300

    set trait

--- app/Http/Models/Customer.php
+++ app/Http/Models/Customer.php
@@ -2,39 +2,14 @@
 
 namespace App\Http\Models;
 
-use App\Http\Models\CustomField;
+use App\Http\Models\Traits\HasCustomFields;
 
 class Customer extends PoloDefaultModel
 {
+	use HasCustomFields;
 	protected $table = "customers";
 	public $casts = [
 		"custom_fields" => "object",
 		"address" => "object",
 	];
-
-	public function setAttribute($key, $value)
-	{
-		$custom_fields = CustomField::where("resource", "clientes")->get()->pluck("field")->toArray();
-		if (!in_array($key, $custom_fields)) return parent::setAttribute($key, $value);
-		return $this->setCustomField($key, $value);
-	}
-
-	public function getAttribute($key)
-	{
-		$custom_fields = CustomField::where("resource", "clientes")->get()->pluck("field")->toArray();
-		if (!in_array($key, $custom_fields)) return parent::getAttribute($key);
-		return $this->getCustomField($key);
-	}
-
-	private function setCustomField($index, $value)
-	{
-		$_data = (object)$this->custom_fields ?? [];
-		$_data->{$index} = $value;
-		$this->custom_fields = $_data;
-	}
-
-	private function getCustomField($index)
-	{
-		return @$this->custom_fields->{$index};
-	}
 }
--- app/Http/Models/Product.php
+++ app/Http/Models/Product.php
@@ -2,7 +2,13 @@
 
 namespace App\Http\Models;
 
+use App\Http\Models\Traits\HasCustomFields;
+
 class Product extends PoloDefaultModel
 {
+	use HasCustomFields;
 	protected $table = "products";
+	public $casts = [
+		"custom_fields" => "object"
+	];
 }
