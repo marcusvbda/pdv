f2fd6bd8826517088f35abd37ae518faf8a544cb
Vinicius Bassalobre <=>
2021-02-28 09:51:10 -0300

    vstack api json

--- resources/js/stores/pdv/actions.js
+++ resources/js/stores/pdv/actions.js
@@ -3,28 +3,34 @@ import api from '~/config/libs/axios'
 export function getProducts({ state, commit }) {
 	commit("setLoadingProducts", true)
 	let params = {
-		per_page: 15,
-		includes: ["tenant"],
-		fields: ["*"],
-		order_by: ["name", "desc"],
-		filters: {
-			where: {
-				name: {
-					like: "%bike%"
-				}, price: {
-					">=": 2100
-				}
-			},
-			where_in: {
-				id: [1]
-			},
-			where_not_in: {
-				id: [3]
-			}
-		}
+		model: "\\App\\Http\\Models\\Product",
+		per_page: 15
 	}
-	api.post("/admin/json-api/produtos", params, { retries: 3 }).then(({ data }) => {
+	api.post("/vstack/json-api", params, { retries: 3 }).then(({ data }) => {
 		commit('setProducts', data)
 		commit("setLoadingProducts", false)
 	})
 }
+
+// let filter_example = {
+// 	model: "\\App\\Http\\Models\\Product",
+// 	per_page: 15,
+// 	includes: ["tenant"],
+// 	fields: ["*"],
+// 	order_by: ["name", "desc"],
+// 	filters: {
+// 		where: {
+// 			name: {
+// 				like: "%bike%"
+// 			}, price: {
+// 				">=": 2100
+// 			}
+// 		},
+// 		where_in: {
+// 			id: [1]
+// 		},
+// 		where_not_in: {
+// 			id: [3]
+// 		}
+// 	}
+// }
