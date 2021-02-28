0c29d574cd2e9e559cbf6194b61816bcc354e04a
Vinicius Bassalobre <=>
2021-02-28 09:41:28 -0300

    loopbackstyle api params

--- resources/js/stores/pdv/actions.js
+++ resources/js/stores/pdv/actions.js
@@ -3,16 +3,27 @@ import api from '~/config/libs/axios'
 export function getProducts({ state, commit }) {
 	commit("setLoadingProducts", true)
 	let params = {
-		per_page: 15
-		// includes: ["tenant"]
-		// name: {
-		// 	"like": "%bike%",
-		// },
-		// price: {
-		// 	">=": 2100
-		// }
+		per_page: 15,
+		includes: ["tenant"],
+		fields: ["*"],
+		order_by: ["name", "desc"],
+		filters: {
+			where: {
+				name: {
+					like: "%bike%"
+				}, price: {
+					">=": 2100
+				}
+			},
+			where_in: {
+				id: [1]
+			},
+			where_not_in: {
+				id: [3]
+			}
+		}
 	}
-	api.post("/admin/produtos/json", params, { retries: 3 }).then(({ data }) => {
+	api.post("/admin/json-api/produtos", params, { retries: 3 }).then(({ data }) => {
 		commit('setProducts', data)
 		commit("setLoadingProducts", false)
 	})
