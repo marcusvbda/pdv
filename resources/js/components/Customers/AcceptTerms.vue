b890498f18388d1cf26337551a032de9e55404f9
Vinicius Bassalobre <=>
2021-03-01 09:35:02 -0300

    termos aceitos e corrige permisspes

--- resources/js/components/Customers/AcceptTerms.vue
+++ resources/js/components/Customers/AcceptTerms.vue
@@ -3,21 +3,24 @@
         <div class="row d-flex align-items-center justify-content-center h-100">
             <div class="col-7">
                 <div class="card" v-loading="loading">
-                    <template v-if="!accepted">
-                        <div class="card-header">Leia atentamente o termo de garantia</div>
-                        <div class="card-body">
-                            <v-runtime-template :template="`<span>${term}</span>`" />
+                    <div class="card-header" v-if="!accepted">
+                       atentamente o termo de garantia
+                    </div>
+                    <div class="card-body">
+                        <v-runtime-template :template="`<span>${term}</span>`" />
+                        <el-alert  v-if="accepted" class="mt-3"
+						title="Obrigado" 
+						type="success" description="O Termo de garantia foi aceito com sucesso !!" 
+						:closable="false" 
+						show-icon
+                        />
+                    </div>
+                    <div class="card-footer" e v-if="!accepted">
+                        <div class="d-flex flex-row justify-content-lg-between align-items-center">
+                            <el-checkbox v-model="checked">Eu, {{ customer.name }}, declaro que li e aceito o termo de garantia</el-checkbox>
+                            <button class="btn btn-sm btn-primary" :disabled="!checked" @click="confirm">Confirmar</button>
                         </div>
-                        <div class="card-footer">
-                            <div class="d-flex flex-row justify-content-lg-between align-items-center">
-                                <el-checkbox v-model="checked">Eu, {{ customer.name }}, declaro que li e aceito o termo de garantia</el-checkbox>
-                                <button class="btn btn-sm btn-primary" :disabled="!checked" @click="confirm">Confirmar</button>
-                            </div>
-                        </div>
-                    </template>
-                    <template v-else>
-                        <el-alert title="Obrigado" type="success" description="O Termo de garantia foi aceito com sucesso !!" :closable="false" show-icon />
-                    </template>
+                    </div>
                 </div>
             </div>
         </div>
@@ -47,8 +50,7 @@ export default {
     },
     methods: {
         confirm() {
-            this.$http.post(`/termo-de-garantia/${this.customer.code}/aceitar`)
-            // .then(() => window.location.reload())
+            this.$http.post(`/termo-de-garantia/${this.customer.code}/aceitar`).then(() => window.location.reload())
         },
     },
 }
